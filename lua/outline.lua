f24351935f8e4cfca18251a9697b6c93c3aaa30f
Josh Winebrener <jwinebrener@blueorigin.com>
2023-11-07 09:20:32 -0600

    Add code outline plugin

---
+++ lua/outline.lua
@@ -0,0 +1,3 @@
+require("symbols-outline").setup()
+vim.keymap.set('n', '<leader>o', ':SymbolsOutline<CR>', { desc = '[O]utline' })
+-- vim.keymap.set('n', '<leader>o', ':SymbolsOutlineClose<CR>', { desc = '[O]utline close' })
