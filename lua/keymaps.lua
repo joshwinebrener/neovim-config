d238635a8c95be2a94bb0043e3e48ebd536263f3
jwinebrener <jwinebrener@blueorigin.com>
2025-05-06 18:12:39 -0500

    Faster save and quit

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -46,8 +46,13 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- Explorer at cwd
 vim.keymap.set('n', '<leader>e', "<cmd>execute 'e %/..'<cr>", { desc = 'Open explorer at cwd' })
 
+-- Quick tab navigation
 vim.keymap.set('n', '<leader>1', '1gt')
 vim.keymap.set('n', '<leader>2', '2gt')
 vim.keymap.set('n', '<leader>3', '3gt')
 vim.keymap.set('n', '<leader>4', '4gt')
 vim.keymap.set('n', '<leader>5', '5gt')
+
+-- Easier save and quit
+vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
+vim.keymap.set('n', '<leader>q', '<cmd>q<cr>')
