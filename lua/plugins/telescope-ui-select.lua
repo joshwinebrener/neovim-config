36f506f93251d38f4410b4fc985a91a5ba5e59d3
jwinebrener <jwinebrener@blueorigin.com>
2025-05-07 19:10:42 -0500

    Add telescope ui select for telescope code actions

---
+++ lua/plugins/telescope-ui-select.lua
@@ -0,0 +1,30 @@
+return {
+  'nvim-telescope/telescope-ui-select.nvim',
+  dependencies = {
+    'telescope.nvim',
+  },
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+        -- even more opts
+      }
+
+      -- pseudo code / specification for writing custom displays, like the one
+      -- for "codeactions"
+      -- specific_opts = {
+      --   [kind] = {
+      --     make_indexed = function(items) -> indexed_items, width,
+      --     make_displayer = function(widths) -> displayer
+      --     make_display = function(displayer) -> function(e)
+      --     make_ordinal = function(e) -> string
+      --   },
+      --   -- for example to disable the custom builtin "codeactions" display
+      --      do the following
+      --   codeactions = false,
+      -- }
+    }
+  },
+  config = function()
+    require("telescope").load_extension("ui-select")
+  end
+}
