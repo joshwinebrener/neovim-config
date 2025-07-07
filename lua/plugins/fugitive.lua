4623a8f8facb8312933740ce0d4c72d1ea0c5d2b
jwinebrener <jwinebrener@blueorigin.com>
2025-07-07 08:52:26 -0500

    Remove old plugin files

--- lua/plugins/fugitive.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'tpope/vim-fugitive',
-  config = function()
-    vim.keymap.set('n', 'ul', '<cmd>diffget \\2<cr>', { desc = '[U]se [L]eft' })
-    vim.keymap.set('n', 'ur', '<cmd>diffget \\3<cr>', { desc = '[U]se [R]eft' })
-  end
-}
